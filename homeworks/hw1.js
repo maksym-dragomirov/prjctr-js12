f3933ddb25306f0d52e119fa5951e5728b9308c9
maksym-dragomirov <maksym.dragomirov@gmail.com>
2024-04-16 00:40:20 +0300

    HW1 - Updates as per comments

--- homeworks/hw1.js
+++ homeworks/hw1.js
@@ -1,16 +1,16 @@
 'use strict';
 
-let x = 5;
-let y = 10;
+const x = 5;
+const y = 10;
 
-let addition = x + y;
-let subtraction = y - x;
-let multiplication = x * y;
-let division = y / x;
-let exponentiation = x ** y;
+const addition = x + y;
+const subtraction = y - x;
+const multiplication = x * y;
+const division = y / x;
+const exponentiation = x ** y;
 
-console.log('Додавання: ' + addition);
-console.log('Віднімання: ' + subtraction);
-console.log('Множення: ' + multiplication);
-console.log('Ділення: ' + division);
-console.log('Зведення в ступінь: ' + exponentiation);
+console.log(`Додавання: ${addition}`);
+console.log(`Віднімання: ${subtraction}`);
+console.log(`Множення: ${multiplication}`);
+console.log(`Ділення: ${division}`);
+console.log(`Зведення в ступінь: ${exponentiation}`);
