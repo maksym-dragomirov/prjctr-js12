d354b30beaa3d48d63b40476a0e57f488ee12f0b
maksym-dragomirov <maksym.dragomirov@gmail.com>
2024-04-28 20:12:22 +0300

    JS12 : Масиви в деталях

---
+++ homeworks/hw4.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * Для вирішення цієї задачі вам буде потрібно згадати про перетворення у рядочкі та числа,
+ * а також використати декілька методів масивів, які ми розбирали на вебінарі.
+ */
+
+// Task 1
+const currentMaxValue = 4589;
+let reverseMaxValue;
+
+// Transforming to string to be able to use SPLIT
+const a = currentMaxValue.toString();
+reverseMaxValue = a.split('').reverse().join('');
+// Without this line we will receive typeof = string instead of number
+reverseMaxValue = Number(reverseMaxValue);
+
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'
+
+
+/**
+ * Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності.
+ * */
+
+// Task 2
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray;
+
+productOfArray = resultsArray.flat(Infinity).reduce((prev, current) => {
+    return prev * current;
+});
+
+console.log(productOfArray); // 24
